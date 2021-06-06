d380398374e7be571e7230cc9f5297417278a8d0
jaseem <65609243+jaseem1999@users.noreply.github.com>
2021-06-06 08:52:28 +0530

    Initial udayy

--- src/Compontents/Committee/Committee.js
+++ src/Compontents/Committee/Committee.js
@@ -8,112 +8,107 @@ function Committee() {
             <div className="Row">
                  <div className ='posters'>
                 <div className= "name">
-                <img className='poster'  src='https://scontent.fcok6-1.fna.fbcdn.net/v/t1.6435-9/190925784_972791526868830_752305930077039657_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=pKXZ57AtEqUAX_duRaL&_nc_ht=scontent.fcok6-1.fna&oh=1443e512330b173a04ecabee3d65f3c2&oe=60DD3B88' alt="fayis" />
+                <img className='posterC'  src='https://scontent.fcok6-1.fna.fbcdn.net/v/t1.6435-9/190925784_972791526868830_752305930077039657_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=pKXZ57AtEqUAX_duRaL&_nc_ht=scontent.fcok6-1.fna&oh=1443e512330b173a04ecabee3d65f3c2&oe=60DD3B88' alt="fayis" />
                   <h1>Fayis</h1>
                   <span>Secretary</span>
                 </div>
                 <div className ="name">
-                <img className='poster'  src='./img/ashiq.jpg' alt="ashiq" />
+                <img className='posterC'  src='./img/ashiq.jpg' alt="ashiq" />
                 <h1>Ashiq</h1>
                 <span>President</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/8c9328e3-5f09-4dd7-bca5-59e414dc3e15.jpg' alt="nazim" />
+                <img className='posterC'  src='./img/8c9328e3-5f09-4dd7-bca5-59e414dc3e15.jpg' alt="nazim" />
                 <h1>Nazim</h1>
                 <span>Joint Secretary</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/bijisha.jpg' alt="bijisha" />
+                <img className='posterC'  src='./img/bijisha.jpg' alt="bijisha" />
                 <h1>Bijisha</h1>
                 <span>Joint Secretary</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/irshad.jpg' alt="irshad" />
+                <img className='posterC'  src='./img/irshad.jpg' alt="irshad" />
                 <h1>Irshad</h1>
                 <span>Vice President</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/akhila.jpg' alt="akhila" />
+                <img className='posterC'  src='./img/akhila.jpg' alt="akhila" />
                 <h1>Akhila</h1>
                 <span>Vice President</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/hari ch.jpg' alt="Hari" />
+                <img className='posterC'  src='./img/hari ch.jpg' alt="Hari" />
                 <h1>Hari Karnan</h1>
                 <span>Secretariat</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/arshad.jpg' alt="arshad" />
+                <img className='posterC'  src='./img/arshad.jpg' alt="arshad" />
                  <h1>Arshad</h1>
                 <span>Secretarite</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/saeed2.jpg' alt="saeed2" />
+                <img className='posterC'  src='./img/saeed2.jpg' alt="saeed2" />
                  <h1>Saeeed</h1>
                 <span>Secretariat</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/IMG_20210107_212637.jpg' alt="shamnad" />
+                <img className='posterC'  src='./img/IMG_20210107_212637.jpg' alt="shamnad" />
                  <h1>Shamnad</h1>
                 <span>Secretariat</span>
                 </div>
                 <div className= "name">
-                <img className='poster'  src='./img/jithin.jpg' alt="jithin" />
+                <img className='posterC'  src='./img/jithin.jpg' alt="jithin" />
                  <h1>Jithin</h1>
                 <span>Secretariat</span>
                 </div>
                  <div className= "name">
-                    <img className='poster' src='https://scontent.fcok2-1.fna.fbcdn.net/v/t1.18169-9/20664967_399838367079848_276977021752220814_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=6YsjS1lpnyQAX_YMHOF&_nc_ht=scontent.fcok2-1.fna&oh=3b0c858edd537ec73fd03ece2f9f136b&oe=60D9B580' alt="jaseem" />
+                    <img className='posterC' src='https://scontent.fcok2-1.fna.fbcdn.net/v/t1.18169-9/20664967_399838367079848_276977021752220814_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=6YsjS1lpnyQAX_YMHOF&_nc_ht=scontent.fcok2-1.fna&oh=3b0c858edd537ec73fd03ece2f9f136b&oe=60D9B580' alt="jaseem" />
                  <h1>Jaseem</h1>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='poster' src='https://casperkruger.co.za/wp-content/uploads/2014/08/Profile-Pic-Demo.png' alt="Bijsh" />
+                    <img className='posterC' src='https://casperkruger.co.za/wp-content/uploads/2014/08/Profile-Pic-Demo.png' alt="Bijsh" />
                  <h1>Bijish</h1>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='poster' src='https://scontent.fcok6-1.fna.fbcdn.net/v/t1.6435-9/184382954_571624010470973_8072761678774991817_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=CVQilpKHL48AX-_fCfC&_nc_ht=scontent.fcok6-1.fna&oh=54368fb15d9dd2c58b8fb13011767feb&oe=60D9286D' alt="Haeesh" />
+                    <img className='posterC' src='https://scontent.fcok6-1.fna.fbcdn.net/v/t1.6435-9/184382954_571624010470973_8072761678774991817_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=CVQilpKHL48AX-_fCfC&_nc_ht=scontent.fcok6-1.fna&oh=54368fb15d9dd2c58b8fb13011767feb&oe=60D9286D' alt="Haeesh" />
                  <h1>Havees</h1>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='poster' src='https://scontent.fcok6-1.fna.fbcdn.net/v/t1.6435-9/169492860_895207738001202_4900647369284746285_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=174925&_nc_ohc=j1jmHTrRbL8AX8yBEQa&_nc_ht=scontent.fcok6-1.fna&oh=e8f286b4e1d2ee1789dd8ee6ca27b9bc&oe=60D7EF91' alt="Hari" />
+                    <img className='posterC' src='https://scontent.fcok2-1.fna.fbcdn.net/v/t1.6435-9/174550626_2937886296537947_1143207611519955889_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=174925&_nc_ohc=fi4bAOVortcAX8urj4M&_nc_ht=scontent.fcok2-1.fna&oh=af5af025bf297f5a598cbd10079c349d&oe=60E24605' alt="Hari" />
                  <h1>Hari</h1>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='poster' src='https://casperkruger.co.za/wp-content/uploads/2014/08/Profile-Pic-Demo.png' alt="Govthem" />
-                 <h1>Goutham</h1>
-                <span>Committee</span>
-                </div>
-                <div className= "name">
-                    <img className='poster' src='https://image.flaticon.com/icons/svg/219/219990.svg' alt="Shabeeba" />
+                    <img className='posterC' src='https://image.flaticon.com/icons/svg/219/219990.svg' alt="Shabeeba" />
                  <h1>Shabeeba</h1>
                 <span>Committee</span>
                 </div>
                  <div className= "name">
-                    <img className='poster' src='https://image.flaticon.com/icons/svg/219/219990.svg' alt="Nurjahana" />
+                    <img className='posterC' src='https://image.flaticon.com/icons/svg/219/219990.svg' alt="Nurjahana" />
                  <h1>Nurjahana</h1>
                 <span>Committee</span>
                 </div>
                  <div className= "name">
-                    <img className='poster' src='https://image.flaticon.com/icons/svg/219/219990.svg' alt="Shibila" />
+                    <img className='posterC' src='https://image.flaticon.com/icons/svg/219/219990.svg' alt="Shibila" />
                  <h1>Shibila</h1>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='poster' src='https://casperkruger.co.za/wp-content/uploads/2014/08/Profile-Pic-Demo.png' alt="Govthem" />
+                    <img className='posterC' src='https://casperkruger.co.za/wp-content/uploads/2014/08/Profile-Pic-Demo.png' alt="Govthem" />
                  <h1>Goutham</h1>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='poster' src='https://casperkruger.co.za/wp-content/uploads/2014/08/Profile-Pic-Demo.png' alt="Ashif" />
+                    <img className='posterC' src='https://casperkruger.co.za/wp-content/uploads/2014/08/Profile-Pic-Demo.png' alt="Ashif" />
                  <h1>Ashif</h1>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='poster' src='./img/uday.jpg' alt="Uday" />
+                    <img className='posterC' src='https://scontent.fccj1-1.fna.fbcdn.net/v/t1.6435-9/186546304_120169060202011_6737104736401103021_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=730e14&_nc_ohc=Abz2ZCVeX00AX8nwBrM&_nc_ht=scontent.fccj1-1.fna&oh=3719807d893defcaaac99aa05ca193be&oe=60E27ECA' alt="Uday" />
                  <h1>Uday</h1>
                 <span>Committee</span>
                 </div>
