2a25c53f4307ca121a2ca5eb7e4e59204b12fd61
Adil <ajkkalappatt@gmail.com>
2021-12-25 18:40:31 +0300

    changed style in nevbar and edited 2

--- src/Compontents/NavBar/Nav.js
+++ src/Compontents/NavBar/Nav.js
@@ -4,9 +4,7 @@ function Nav() {
     return (
         <div className="Nav">
             <img className="logo-kc" src="./img/mes-sfi.png" alt="sfikc" />
-               <div className="sfi">
-                    <a href="http://www.sficec.org/"><h1>Student's federation of India</h1></a>
-               </div>
+               
              <img className="logo-cc" src="./img/50sfi.png" alt="sficc" />
              <div className="fade-top"></div>
         </div>
