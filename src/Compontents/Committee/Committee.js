1828d3c0dc507ae06a5efcf532973dae40e05d1e
Adil <ajkkalappatt@gmail.com>
2022-02-20 13:56:06 +0300

    2/20/2022 committee 22 edited

--- src/Compontents/Committee/Committee.js
+++ src/Compontents/Committee/Committee.js
@@ -13,22 +13,22 @@ function Committee() {
             <div className="Row">
                  <div className ='posters'>
                 <div className= "name">
-                <img className='posterC'  src='./img/shyam2.jpg' alt="shyam" />
+                <img className='posterC'  src='./img/Shyam2.jpg' alt="shyam" />
                   <h2>Shyam</h2>
                   <span>Secretary</span>
                 </div>
                 <div className ="name">
-                <img className='posterC'  src='./img/bijeesh.jpg' alt="ashiq" />
+                <img className='posterC'  src='./img/Bijeesh.jpg' alt="ashiq" />
                 <h2>Bijeesh</h2>
                 <span>President</span>
                 </div>
                 <div className= "name">
-                <img className='posterC'  src='./img/uday2.jpg' alt="nazim" />
+                <img className='posterC'  src='./img/Uday2.jpg' alt="nazim" />
                 <h2>Uday</h2>
                 <span>Joint Secretary</span>
                 </div>
                 <div className= "name">
-                <img className='posterC'  src='./img/shabeeba.jpg' alt="bijisha" />
+                <img className='posterC'  src='./img/Shabeeba.jpg' alt="bijisha" />
                 <h2>Shabeeba</h2>
                 <span>Joint Secretary</span>
                 </div>
@@ -38,7 +38,7 @@ function Committee() {
                 <span>Vice President</span>
                 </div>
                 <div className= "name">
-                <img className='posterC'  src='./img/swathi.jpg' alt="akhila" />
+                <img className='posterC'  src='./img/Swathi.jpg' alt="akhila" />
                 <h2>Swathi</h2>
                 <span>Vice President</span>
                 </div>
@@ -49,11 +49,11 @@ function Committee() {
                 </div>
                 <div className= "name">
                 <img className='posterC'  src='./img/jenneth2.jpg' alt="arshad" />
-                 <h2>Arshad</h2>
+                 <h2>Jennath</h2>
                 <span>Secretarite</span>
                 </div>
                 <div className= "name">
-                <img className='posterC'  src='./img/haseeb.jpg' alt="saeed2" />
+                <img className='posterC'  src='./img/Haseeb.jpg' alt="saeed2" />
                  <h2>haseeb</h2>
                 <span>Secretariat</span>
                 </div>
@@ -63,38 +63,38 @@ function Committee() {
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                <img className='posterC'  src='./img/ruksana.jpg' alt="jithin" />
+                <img className='posterC'  src='./img/Ruksana.jpg' alt="jithin" />
                  <h2>Ruksana</h2>
                 <span>Committee</span>
                 </div>
                  
                 <div className= "name">
-                    <img className='posterC' src='./img/renya.jpg' alt="Bijsh" />
+                    <img className='posterC' src='./img/Renya.jpg' alt="Bijsh" />
                  <h2>Renya</h2>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='posterC' src='./img/riyan.jpg' alt="Haeesh" />
+                    <img className='posterC' src='./img/Riyan.jpg' alt="Haeesh" />
                  <h2>Riyan</h2>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='posterC' src='./img/vineeth.jpg' alt="Hari" />
+                    <img className='posterC' src='./img/Vineeth.jpg' alt="Hari" />
                  <h2>Vineeth</h2>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='posterC' src='./img/shijas.jpg' alt="Shabeeba" />
+                    <img className='posterC' src='./img/Shijas.jpg' alt="Shabeeba" />
                  <h2>Shijas</h2>
                 <span>Committee</span>
                 </div>
                  <div className= "name">
-                    <img className='posterC' src='./img/shifa.jpg' alt="Nurjahana" />
+                    <img className='posterC' src='./img/Shifa.jpg' alt="Nurjahana" />
                  <h2>Shifa</h2>
                 <span>Committee</span>
                 </div>
                  <div className= "name">
-                    <img className='posterC' src='./img/ubaid.jpg' alt="Shibila" />
+                    <img className='posterC' src='./img/Ubaid.jpg' alt="Shibila" />
                  <h2>Ubaid</h2>
                 <span>Committee</span>
                 </div>
@@ -104,12 +104,12 @@ function Committee() {
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='posterC' src='./img/jihad.jpg' alt="Ashif" />
+                    <img className='posterC' src='./img/Jihad.jpg' alt="Ashif" />
                  <h2>Jihad</h2>
                 <span>Committee</span>
                 </div>
                 <div className= "name">
-                    <img className='posterC' src='./img/arya.jpg' alt="Uday" />
+                    <img className='posterC' src='./img/Arya.jpg' alt="Uday" />
                  <h2>Arya</h2>
                 <span>Committee</span>
                 </div>
