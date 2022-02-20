b17d1e25b518a8106dd24c822d64ac5c8d4b2135
Adil <ajkkalappatt@gmail.com>
2022-02-20 13:33:27 +0300

    2/20/2022 committee 22

--- src/Compontents/Committee/Committee.js
+++ src/Compontents/Committee/Committee.js
@@ -9,8 +9,119 @@ function Committee() {
     
     return (
         <div>
-             <h2> Committee 2021</h2>
+             <h2> Committee 2022</h2>
             <div className="Row">
+                 <div className ='posters'>
+                <div className= "name">
+                <img className='posterC'  src='./img/shyam2.jpg' alt="shyam" />
+                  <h2>Shyam</h2>
+                  <span>Secretary</span>
+                </div>
+                <div className ="name">
+                <img className='posterC'  src='./img/bijeesh.jpg' alt="ashiq" />
+                <h2>Bijeesh</h2>
+                <span>President</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/uday2.jpg' alt="nazim" />
+                <h2>Uday</h2>
+                <span>Joint Secretary</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/shabeeba.jpg' alt="bijisha" />
+                <h2>Shabeeba</h2>
+                <span>Joint Secretary</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/arshad.jpg' alt="irshad" />
+                <h2>Arshad</h2>
+                <span>Vice President</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/swathi.jpg' alt="akhila" />
+                <h2>Swathi</h2>
+                <span>Vice President</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/haveesh.jpg' alt="Hari" />
+                <h2>Haveesh</h2>
+                <span>Secretariat</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/jenneth2.jpg' alt="arshad" />
+                 <h2>Arshad</h2>
+                <span>Secretarite</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/haseeb.jpg' alt="saeed2" />
+                 <h2>haseeb</h2>
+                <span>Secretariat</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/ashif.jpg' alt="shamnad" />
+                 <h2>Ashif</h2>
+                <span>Committee</span>
+                </div>
+                <div className= "name">
+                <img className='posterC'  src='./img/ruksana.jpg' alt="jithin" />
+                 <h2>Ruksana</h2>
+                <span>Committee</span>
+                </div>
+                 
+                <div className= "name">
+                    <img className='posterC' src='./img/renya.jpg' alt="Bijsh" />
+                 <h2>Renya</h2>
+                <span>Committee</span>
+                </div>
+                <div className= "name">
+                    <img className='posterC' src='./img/riyan.jpg' alt="Haeesh" />
+                 <h2>Riyan</h2>
+                <span>Committee</span>
+                </div>
+                <div className= "name">
+                    <img className='posterC' src='./img/vineeth.jpg' alt="Hari" />
+                 <h2>Vineeth</h2>
+                <span>Committee</span>
+                </div>
+                <div className= "name">
+                    <img className='posterC' src='./img/shijas.jpg' alt="Shabeeba" />
+                 <h2>Shijas</h2>
+                <span>Committee</span>
+                </div>
+                 <div className= "name">
+                    <img className='posterC' src='./img/shifa.jpg' alt="Nurjahana" />
+                 <h2>Shifa</h2>
+                <span>Committee</span>
+                </div>
+                 <div className= "name">
+                    <img className='posterC' src='./img/ubaid.jpg' alt="Shibila" />
+                 <h2>Ubaid</h2>
+                <span>Committee</span>
+                </div>
+                <div className= "name">
+                    <img className='posterC' src='./img/noor.jpeg' alt="Govthem" />
+                 <h2>Noorjahan</h2>
+                <span>Committee</span>
+                </div>
+                <div className= "name">
+                    <img className='posterC' src='./img/jihad.jpg' alt="Ashif" />
+                 <h2>Jihad</h2>
+                <span>Committee</span>
+                </div>
+                <div className= "name">
+                    <img className='posterC' src='./img/arya.jpg' alt="Uday" />
+                 <h2>Arya</h2>
+                <span>Committee</span>
+                </div>
+              
+              
+                
+             
+            </div>
+            
+            </div>
+            <h2> Committee 2021</h2>
+                <div className="Row">
                  <div className ='posters'>
                 <div className= "name">
                 <img className='posterC'  src='./img/fays.jpg' alt="fayis" />
@@ -122,6 +233,7 @@ function Committee() {
                 
              
             </div>
+            
             </div>
            
             
