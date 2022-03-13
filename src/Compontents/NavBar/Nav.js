d787fa9607a9c2787d7aa3c2f060b7d4282fb133
Adil <ajkkalappatt@gmail.com>
2022-03-13 21:02:10 +0530

    3/13/2022 added state committee logo

--- src/Compontents/NavBar/Nav.js
+++ src/Compontents/NavBar/Nav.js
@@ -4,7 +4,10 @@ function Nav() {
     return (
         <div className="Nav">
             <img className="logo-kc" src="./img/mes-sfi.png" alt="sfikc" />
-               
+            <div className="nav-links"  id="center"> 
+                <img className='logo-st' src="./img/sfikeralalogo.png" alt="sfikerala" />
+            </div>
+             
              <img className="logo-cc" src="./img/50sfi.png" alt="sficc" />
              <div className="fade-top"></div>
         </div>
